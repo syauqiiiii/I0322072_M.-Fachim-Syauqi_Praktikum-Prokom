11c0eff0f48b1736db1814163ac43d17adc1b82e
syauqiiiii <fachimsyauqi@student.uns.ac.id>
2023-03-14 23:02:57 +0700

    mengumpulkan tugas Excercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,6 @@
+print("Halo nama aku M. Fachim Syauqi")
+print("NIM aku adalah I0322072")
+print("Aku tinggal di Banjarnegara")
+print("Banjarnegara merupakan sebuah kabupaten yang ada di Jawa Tengah")
+print("Di daerah saya masih banyak terdapat kegiatan yang menyebabkan warganya bergotong royong")
+print("Harapan saya ketika lulus dari Teknik Industri UNS adalah saya bisa mengamalkan ilmu yang saya dapat di Teknik Industri UNS dan bisa menjadi orang yang berguna bagi bangsa dan negara")
